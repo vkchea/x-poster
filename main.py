5636c7705fdd2d8e9b39bc0a52092ccf9b63aa80
Virath Chea <skennethchea@gmail.com>
2025-01-28 21:13:03 -0500

    Created logic to create tweet on X account. Created gitignore file to ignore unnecessary files.

--- main.py
+++ main.py
@@ -1,62 +1,75 @@
-import tweepy
+import base64
+import hashlib
+import os
+import re
+import requests
 
-def post_to_x(text, picture_path, emojis):
-    """
-    Posts a tweet with text, an image, and a list of emojis.
+from requests_oauthlib import OAuth2Session
+from flask import Flask, request, redirect, session, url_for, render_template
 
-    Parameters:
-        text (str): The text content of the tweet.
-        picture_path (str): The file path of the picture to attach.
-        emojis (list): A list of emojis to append to the tweet.
-    """
-    """
-        Retrieve API and Access token information from a text file.
-        1st line = API_KEY
-        2nd line = API_SECRET
-        3rd line = ACCESS_TOKEN
-        4th line = ACCESS_TOKEN_SECRET
-        5th line = API_BEARER_TOKEN 
-    """
-    access_list = []
-    with open('Files/access.txt', 'r') as file:
-        lines = file.readlines()
+app = Flask(__name__)
+app.secret_key = os.urandom(50)
 
-    for line in lines:
-        access_list.append(line.strip())
+client_id=''
+client_secret=''
+redirect_uri='http://127.0.0.1:5000/oauth/callback'
 
-    API_KEY = access_list[0]
-    API_SECRET = access_list[1]
-    ACCESS_TOKEN = access_list[2]
-    ACCESS_TOKEN_SECRET = access_list[3]
-    API_BEARER_TOKEN = access_list[4]
+auth_url = "https://twitter.com/i/oauth2/authorize"
+token_url = "https://api.x.com/2/oauth2/token"
 
-#     # Authenticate to Twitter
-    auth = tweepy.OAuth1UserHandler(consumer_key=API_KEY,
-                                    consumer_secret=API_SECRET,
-                                    access_token=ACCESS_TOKEN,
-                                    access_token_secret=ACCESS_TOKEN_SECRET)
-    api = tweepy.API(auth)
+scopes = ["tweet.read", "users.read", "tweet.write"]
 
-    try:
-#         # Combine text and emojis into a single string
-        emojis_str = " ".join(emojis)
-        tweet_content = f"{text} {emojis_str}"
+code_verifier = base64.urlsafe_b64encode(os.urandom(30)).decode("utf-8")
+code_verifier = re.sub("[^a-zA-Z0-9]+", "", code_verifier)
 
-        # Post the tweet with an image
-        if picture_path:
-            api.update_status_with_media(status=tweet_content, filename=picture_path)
-        else:
-            api.update_status(status=tweet_content)
+code_challenge = hashlib.sha256(code_verifier.encode("utf-8")).digest()
+code_challenge = base64.urlsafe_b64encode(code_challenge).decode("utf-8")
+code_challenge = code_challenge.replace("=", "")
 
-        print("Tweet posted successfully!")
+API_KEY=''
+API_SECRET=''
+ACCESS_TOKEN=''
+ACCESS_TOKEN_SECRET=''
 
-    except Exception as e:
-        print(f"An error occurred: {e}")
+def post_tweet(payload, new_token):
+    print("Tweeting!")
+    return requests.request(
+        "POST",
+        "https://api.x.com/2/tweets",
+        json=payload,
+        headers={
+            "Authorization": "Bearer {}".format(new_token["access_token"]),
+            "Content-Type": "application/json",
+        },
+    )
 
-# # Example usage
+@app.route("/")
+def demo():
+    global twitter
+    twitter = OAuth2Session(client_id, redirect_uri=redirect_uri, scope=scopes)
+    authorization_url, state = twitter.authorization_url(
+        auth_url, code_challenge=code_challenge, code_challenge_method="S256"
+    )
+    session["oauth_state"] = state
+    return redirect(authorization_url)
+
+@app.route("/oauth/callback", methods=["GET"])
+def callback():
+    code = request.args.get("code")
+    print(request)
+    token = twitter.fetch_token(
+        token_url=token_url,
+        client_secret=client_secret,
+        code_verifier=code_verifier,
+        code=code,
+    )
+    print("token: ", token)
+    payload = {
+        "text": "Learn how to use the user Tweet timeline and user mention timeline endpoints in the X API v2 to explore Tweet https://t.co/56a0vZUx7i"
+    }
+    response = post_tweet(payload, token).json()
+    return response
+
+
 if __name__ == "__main__":
-    text = "Check out my new post!"
-    picture_path = ""  # Provide the correct path to your image
-    emojis = ["🚀", "🌟", "📸"]  # List of emojis
-
-    post_to_x(text, picture_path, emojis)
+    app.run()
